7769b8595634e8ef85ca10d7a3f835697dd267f4
Daniel Moldovan <dmoldovan@softvision.ro>
2020-09-10 11:20:43 +0300

    My first commit

---
+++ dotnet-cosmosdb-quickstart/Startup.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using todo.Models;
+
+namespace todo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Adds services required for using options.
+            services.AddOptions();
+
+            // Register the IConfiguration instance 
+            services.Configure<DocDbConfig>(Configuration);
+
+            //Register the Item service
+            services.AddTransient<IItemService, ItemService>();
+            // Add framework services.
+            var mvcBuilder = services.AddControllersWithViews();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+
+
+#if DEBUG
+            mvcBuilder.AddRazorRuntimeCompilation();
+#endif
+
+            services.AddApplicationInsightsTelemetry("05ec027c-c9fd-4763-9eb0-4f2b212547bf");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            //loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Item/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseStatusCodePagesWithReExecute("/error/{0}");
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Item}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
